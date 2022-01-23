2ae83b8776193f1fe939474cf4f1053750afeee9
Benjamin Arsenault <hi@benjamin.dev>
2022-01-22 21:06:13 -0500

    Remove unecessary complexity

--- check-forsale.py
+++ check-forsale.py
@@ -21,39 +21,3 @@ def get_portfolios(directory):
   return fs_portfolios
 
 
-def set_loop_arguments(initial_amount_portfolios, portfolios, current_check):
-
-
-  # check if first iteration
-  if len(portfolios) == initial_amount_portfolios:
-
-    # no append
-    current_check = portfolios.pop(0)
-  
-  else:
-
-    # put portfolio back at end of array
-    portfolios.append(current_check)
-
-    # pop new current
-    current_check = portfolios.pop(0)
-
-
-
-
-
-
-def run_cleaner():
-
-  # get portfolios
-  fs_portfolios = get_portfolios(fs_portfolios_directory)
-
-  # keep initial amount of portfolios
-
-  # needed for if/else statement
-  current_check = fs_portfolios[0]
-
-  initial_amount_portfolios = len(fs_portfolios)
-
-  # set loop arguments
-  set_loop_arguments(initial_amount_portfolios, fs_portfolios, current_check)
