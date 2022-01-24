052baf484a74e47fe7b47fbc3a2e219b6269c041
Benjamin Arsenault <hi@benjamin.dev>
2022-01-24 11:09:28 -0500

    Add function for turning directory into list of dataframes

--- check-forsale.py
+++ check-forsale.py
@@ -1,10 +1,7 @@
 # Check "for sale" portfolios against each other for duplicate accounts.
 
 # Approach
-# - grab all portfolios in the for-sale directory, store them as a list.
-# - pull out the first portfolio and remove it from portfolios list.
-# - run through all portfolios and check them against current check portfolio.
-# - once completed, append to end of list, pop portfolios[0] to current check, repeat.
+# - Make dataframes from directory
 
 import pandas as pd
 import os
@@ -18,22 +15,42 @@ fs_portfolios = os.listdir(fs_directory_path)
 # Store amount of files for loop
 files_to_clean = len(fs_portfolios)
 
-for i in range(files_to_clean):
 
-  print(fs_portfolios)
+def make_dataframes(directory):
 
-  # Current portfolio in loop
-  current_portfolio = fs_directory_path + fs_portfolios[0]
+  # get list of file names in given directory
+  file_names = os.listdir(directory)
 
-  # read current portfolio as df
-  # current_portfolio_df = pd.read_excel(current_portfolio)
+  # make empty list to hold dataframes
+  dataframes = []
+
+  # make files readable to pandas
+  for file in file_names:
+    
+    # add directory prefix
+    file = fs_directory_path + file
+
+    # read as df
+    df = pd.read_excel(file)
+    
+    # store as dataframes
+    dataframes.append(df)
+
+  print(dataframes[0])
+
+
+
+
+# for i in range(files_to_clean):
+
+#   # get data frames, single current portfolio and then list of portfolios to compare
+#   get_dataframes(fs_portfolios[0], fs_portfolios)
+#   # current_portfolio_df = pd.read_excel(current_portfolio)
   
-  # remove current portfolio from list
-  current_portfolio_name = fs_portfolios.pop(0)
+#   # remove current portfolio from list
+#   current_portfolio_name = fs_portfolios.pop(0)
 
-  # clean
+#   # append back
+#   fs_portfolios.append(current_portfolio_name)
 
-  # append back
-  fs_portfolios.append(current_portfolio_name)
-
-print(fs_portfolios)
+make_dataframes(fs_directory_path)
